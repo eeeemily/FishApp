151bef380f530147184e6a1990519d537f100917
Emily Zheng <mzheng9@u.rochester.edu>
2021-12-06 15:23:21 -0500

    done with basic layout, trying to get rid of navigation controller to have tab bar controller

--- FishApp/SceneDelegate.swift
+++ FishApp/SceneDelegate.swift
@@ -19,13 +19,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         loadFishes()
-        if let navController = window?.rootViewController as? UINavigationController{
-            if let vc = navController.viewControllers.last as? FishTVC{
-                vc.fishInfo = fishInfo
-                print("new!")
-            }
+//        if let navController = window?.rootViewController as? UINavigationController{
+//            if let vc = navController.viewControllers.last as? FishTVC{
+//                vc.fishInfo = fishInfo
+//                print("new!")
+//            }
+//        }
+        if let vc = self.window?.rootViewController as? FishTVC {
+            vc.fishInfo = fishInfo
+            print("hello")
+
         }
-        
         }
     
 
